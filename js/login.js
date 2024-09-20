45ada8014b5a11c4d3f61e77f6625ceb6e11785c
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-09-20 12:40:12 +0600

    Get pin number and validate pin and phone number then set alert

--- js/login.js
+++ js/login.js
@@ -5,6 +5,18 @@ document.getElementById('login_button').addEventListener('click', function (even
 
     // Get mobile number.
     const mobileNumber = document.getElementById('mobile_number_field').value;
-    console.log(mobileNumber);
+
+    // Get pin number.
+    const pinNumber = document.getElementById('pin_number_field').value;
+    console.log(mobileNumber, pinNumber);
+
+
+    // Validate phone and pin number. This is not ideal way. It's a bad way.
+    if (mobileNumber === '018' && pinNumber === '123') {
+        console.log('log in');
+    }
+    else {
+        alert('Wrong mobile number or pin. Please enter correct mobile number and pin');
+    }
 
 })
