e1a9af3ff7a15fcad409c97c725d7598f2b5e84d
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-09-22 18:11:08 +0600

    Hide all section without latest payment section in homepage.

--- js/home.js
+++ js/home.js
@@ -1,3 +1,14 @@
+document.getElementById('latest_payment_section').classList.remove('hidden');
+
+document.getElementById('add_money_section').classList.add('hidden');
+document.getElementById('cash_out_section').classList.add('hidden');
+document.getElementById('transfer_money_section').classList.add('hidden');
+document.getElementById('get_bonus_section').classList.add('hidden');
+document.getElementById('pay_bill_section').classList.add('hidden');
+document.getElementById('transactions_history_section').classList.add('hidden');
+
+
+
 // Add money section.
 document.getElementById('add_money_button')
     .addEventListener('click', function (event) {
