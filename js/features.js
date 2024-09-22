3a206597511800aa266e59238e880923c4fefe47
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-09-22 18:31:47 +0600

    Features button interactive after click

--- js/features.js
+++ js/features.js
@@ -1,6 +1,6 @@
 // Add money.
 document.getElementById('add_money_btn')
-    .addEventListener('click', function(){
+    .addEventListener('click', function () {
         document.getElementById('latest_payment_section').classList.add('hidden');
 
         document.getElementById('add_money_section').classList.remove('hidden');
@@ -10,53 +10,168 @@ document.getElementById('add_money_btn')
         document.getElementById('get_bonus_section').classList.add('hidden');
         document.getElementById('pay_bill_section').classList.add('hidden');
         document.getElementById('transactions_history_section').classList.add('hidden');
+
+
+        // Button bg, border, text color after click.
+        document.getElementById('add_money_btn').style.borderColor = "rgb(8, 116, 242)";
+        document.getElementById('add_money_btn').style.backgroundColor = "rgba(8, 116, 242, 0.05)";
+        document.getElementById('add_money_btn').style.color = "rgb(8, 116, 242)";
+
+
+
+
+        document.getElementById('pay_bill_btn').style.borderColor = "";
+        document.getElementById('pay_bill_btn').style.backgroundColor = "";
+        document.getElementById('pay_bill_btn').style.color = "";
+
+        document.getElementById('get_bonus_btn').style.borderColor = "";
+        document.getElementById('get_bonus_btn').style.backgroundColor = "";
+        document.getElementById('get_bonus_btn').style.color = "";
+
+        document.getElementById('transfer_money_btn').style.borderColor = "";
+        document.getElementById('transfer_money_btn').style.backgroundColor = "";
+        document.getElementById('transfer_money_btn').style.color = "";
+
+        document.getElementById('cash_out_btn').style.borderColor = "";
+        document.getElementById('cash_out_btn').style.backgroundColor = "";
+        document.getElementById('cash_out_btn').style.color = "";
+
+        document.getElementById('transactions_btn').style.borderColor = "";
+        document.getElementById('transactions_btn').style.backgroundColor = "";
+        document.getElementById('transactions_btn').style.color = "";
     })
 
 // Cash out.
 document.getElementById('cash_out_btn')
-    .addEventListener('click', function(){
+    .addEventListener('click', function () {
         document.getElementById('latest_payment_section').classList.add('hidden');
         document.getElementById('add_money_section').classList.add('hidden');
 
         document.getElementById('cash_out_section').classList.remove('hidden');
-        
+
         document.getElementById('transfer_money_section').classList.add('hidden');
         document.getElementById('get_bonus_section').classList.add('hidden');
         document.getElementById('pay_bill_section').classList.add('hidden');
         document.getElementById('transactions_history_section').classList.add('hidden');
+
+
+        // Button bg, border, text color after click.
+        document.getElementById('cash_out_btn').style.borderColor = "rgb(8, 116, 242)";
+        document.getElementById('cash_out_btn').style.backgroundColor = "rgba(8, 116, 242, 0.05)";
+        document.getElementById('cash_out_btn').style.color = "rgb(8, 116, 242)";
+
+        
+
+        document.getElementById('pay_bill_btn').style.borderColor = "";
+        document.getElementById('pay_bill_btn').style.backgroundColor = "";
+        document.getElementById('pay_bill_btn').style.color = "";
+
+        document.getElementById('get_bonus_btn').style.borderColor = "";
+        document.getElementById('get_bonus_btn').style.backgroundColor = "";
+        document.getElementById('get_bonus_btn').style.color = "";
+
+        document.getElementById('transfer_money_btn').style.borderColor = "";
+        document.getElementById('transfer_money_btn').style.backgroundColor = "";
+        document.getElementById('transfer_money_btn').style.color = "";
+
+        document.getElementById('add_money_btn').style.borderColor = "";
+        document.getElementById('add_money_btn').style.backgroundColor = "";
+        document.getElementById('add_money_btn').style.color = "";
+
+        document.getElementById('transactions_btn').style.borderColor = "";
+        document.getElementById('transactions_btn').style.backgroundColor = "";
+        document.getElementById('transactions_btn').style.color = "";
     })
 
 // Transfer money.
 document.getElementById('transfer_money_btn')
-    .addEventListener('click', function(){
+    .addEventListener('click', function () {
         document.getElementById('latest_payment_section').classList.add('hidden');
         document.getElementById('add_money_section').classList.add('hidden');
         document.getElementById('cash_out_section').classList.add('hidden');
 
         document.getElementById('transfer_money_section').classList.remove('hidden');
 
-        document.getElementById('get_bonus_section').classList.add('hidden');        
+        document.getElementById('get_bonus_section').classList.add('hidden');
         document.getElementById('pay_bill_section').classList.add('hidden');
         document.getElementById('transactions_history_section').classList.add('hidden');
+
+
+        // Button bg, border, text color after click.
+        document.getElementById('transfer_money_btn').style.borderColor = "rgb(8, 116, 242)";
+        document.getElementById('transfer_money_btn').style.backgroundColor = "rgba(8, 116, 242, 0.05)";
+        document.getElementById('transfer_money_btn').style.color = "rgb(8, 116, 242)";
+
+
+
+        document.getElementById('pay_bill_btn').style.borderColor = "";
+        document.getElementById('pay_bill_btn').style.backgroundColor = "";
+        document.getElementById('pay_bill_btn').style.color = "";
+
+        document.getElementById('get_bonus_btn').style.borderColor = "";
+        document.getElementById('get_bonus_btn').style.backgroundColor = "";
+        document.getElementById('get_bonus_btn').style.color = "";
+
+        document.getElementById('cash_out_btn').style.borderColor = "";
+        document.getElementById('cash_out_btn').style.backgroundColor = "";
+        document.getElementById('cash_out_btn').style.color = "";
+
+        document.getElementById('add_money_btn').style.borderColor = "";
+        document.getElementById('add_money_btn').style.backgroundColor = "";
+        document.getElementById('add_money_btn').style.color = "";
+
+        document.getElementById('transactions_btn').style.borderColor = "";
+        document.getElementById('transactions_btn').style.backgroundColor = "";
+        document.getElementById('transactions_btn').style.color = "";
     })
 
 // Get bonus.
 document.getElementById('get_bonus_btn')
-    .addEventListener('click', function(){
+    .addEventListener('click', function () {
         document.getElementById('latest_payment_section').classList.add('hidden');
         document.getElementById('add_money_section').classList.add('hidden');
         document.getElementById('cash_out_section').classList.add('hidden');
         document.getElementById('transfer_money_section').classList.add('hidden');
 
         document.getElementById('get_bonus_section').classList.remove('hidden');
-        
+
         document.getElementById('pay_bill_section').classList.add('hidden');
         document.getElementById('transactions_history_section').classList.add('hidden');
+
+
+
+        // Button bg, border, text color after click.
+        document.getElementById('get_bonus_btn').style.borderColor = "rgb(8, 116, 242)";
+        document.getElementById('get_bonus_btn').style.backgroundColor = "rgba(8, 116, 242, 0.05)";
+        document.getElementById('get_bonus_btn').style.color = "rgb(8, 116, 242)";
+
+
+
+
+        document.getElementById('pay_bill_btn').style.borderColor = "";
+        document.getElementById('pay_bill_btn').style.backgroundColor = "";
+        document.getElementById('pay_bill_btn').style.color = "";
+
+        document.getElementById('transfer_money_btn').style.borderColor = "";
+        document.getElementById('transfer_money_btn').style.backgroundColor = "";
+        document.getElementById('transfer_money_btn').style.color = "";
+
+        document.getElementById('cash_out_btn').style.borderColor = "";
+        document.getElementById('cash_out_btn').style.backgroundColor = "";
+        document.getElementById('cash_out_btn').style.color = "";
+
+        document.getElementById('add_money_btn').style.borderColor = "";
+        document.getElementById('add_money_btn').style.backgroundColor = "";
+        document.getElementById('add_money_btn').style.color = "";
+
+        document.getElementById('transactions_btn').style.borderColor = "";
+        document.getElementById('transactions_btn').style.backgroundColor = "";
+        document.getElementById('transactions_btn').style.color = "";
     })
 
 // Pay bill.
 document.getElementById('pay_bill_btn')
-    .addEventListener('click', function(){
+    .addEventListener('click', function () {
         document.getElementById('latest_payment_section').classList.add('hidden');
         document.getElementById('add_money_section').classList.add('hidden');
         document.getElementById('cash_out_section').classList.add('hidden');
@@ -64,13 +179,45 @@ document.getElementById('pay_bill_btn')
         document.getElementById('get_bonus_section').classList.add('hidden');
 
         document.getElementById('pay_bill_section').classList.remove('hidden');
-        
+
         document.getElementById('transactions_history_section').classList.add('hidden');
+
+
+
+
+        // Button bg, border, text color after click.
+        document.getElementById('pay_bill_btn').style.borderColor = "rgb(8, 116, 242)";
+        document.getElementById('pay_bill_btn').style.backgroundColor = "rgba(8, 116, 242, 0.05)";
+        document.getElementById('pay_bill_btn').style.color = "rgb(8, 116, 242)";
+
+
+
+
+
+        document.getElementById('get_bonus_btn').style.borderColor = "";
+        document.getElementById('get_bonus_btn').style.backgroundColor = "";
+        document.getElementById('get_bonus_btn').style.color = "";
+
+        document.getElementById('transfer_money_btn').style.borderColor = "";
+        document.getElementById('transfer_money_btn').style.backgroundColor = "";
+        document.getElementById('transfer_money_btn').style.color = "";
+
+        document.getElementById('cash_out_btn').style.borderColor = "";
+        document.getElementById('cash_out_btn').style.backgroundColor = "";
+        document.getElementById('cash_out_btn').style.color = "";
+
+        document.getElementById('add_money_btn').style.borderColor = "";
+        document.getElementById('add_money_btn').style.backgroundColor = "";
+        document.getElementById('add_money_btn').style.color = "";
+
+        document.getElementById('transactions_btn').style.borderColor = "";
+        document.getElementById('transactions_btn').style.backgroundColor = "";
+        document.getElementById('transactions_btn').style.color = "";
     })
-    
+
 // Transaction History.
 document.getElementById('transactions_btn')
-    .addEventListener('click', function(){
+    .addEventListener('click', function () {
         document.getElementById('latest_payment_section').classList.add('hidden');
         document.getElementById('add_money_section').classList.add('hidden');
         document.getElementById('cash_out_section').classList.add('hidden');
@@ -79,4 +226,40 @@ document.getElementById('transactions_btn')
         document.getElementById('pay_bill_section').classList.add('hidden');
 
         document.getElementById('transactions_history_section').classList.remove('hidden');
+
+
+
+
+
+
+
+        // Button bg, border, text color after click.
+        document.getElementById('transactions_btn').style.borderColor = "rgb(8, 116, 242)";
+        document.getElementById('transactions_btn').style.backgroundColor = "rgba(8, 116, 242, 0.05)";
+        document.getElementById('transactions_btn').style.color = "rgb(8, 116, 242)";
+        
+        
+        
+        
+        
+        
+        document.getElementById('pay_bill_btn').style.borderColor = "";
+        document.getElementById('pay_bill_btn').style.backgroundColor = "";
+        document.getElementById('pay_bill_btn').style.color = "";
+
+        document.getElementById('get_bonus_btn').style.borderColor = "";
+        document.getElementById('get_bonus_btn').style.backgroundColor = "";
+        document.getElementById('get_bonus_btn').style.color = "";
+
+        document.getElementById('transfer_money_btn').style.borderColor = "";
+        document.getElementById('transfer_money_btn').style.backgroundColor = "";
+        document.getElementById('transfer_money_btn').style.color = "";
+
+        document.getElementById('cash_out_btn').style.borderColor = "";
+        document.getElementById('cash_out_btn').style.backgroundColor = "";
+        document.getElementById('cash_out_btn').style.color = "";
+
+        document.getElementById('add_money_btn').style.borderColor = "";
+        document.getElementById('add_money_btn').style.backgroundColor = "";
+        document.getElementById('add_money_btn').style.color = "";
     })
