d52bda7a65ad2e372a4344c2502d1b21554b9ea8
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-09-22 17:53:44 +0600

    All section design and partialy toggle features complete

---
+++ js/features.js
@@ -0,0 +1,54 @@
+// Add money.
+document.getElementById('add_money_btn')
+    .addEventListener('click', function(){
+        document.getElementById('add_money_section').classList.remove('hidden');
+
+        document.getElementById('latest_payment_section').classList.add('hidden');
+        document.getElementById('cash_out_section').classList.add('hidden');
+        document.getElementById('get_bonus_section').classList.add('hidden');
+        document.getElementById('pay_bill_section').classList.add('hidden');
+    })
+
+// Cash out.
+document.getElementById('cash_out_btn')
+    .addEventListener('click', function(){
+        document.getElementById('cash_out_section').classList.remove('hidden');
+        
+        document.getElementById('add_money_section').classList.add('hidden');
+        document.getElementById('latest_payment_section').classList.add('hidden');
+        document.getElementById('get_bonus_section').classList.add('hidden');
+        document.getElementById('pay_bill_section').classList.add('hidden');
+    })
+
+// Transfer money.
+document.getElementById('transfer_money_btn')
+    .addEventListener('click', function(){
+        document.getElementById('get_bonus_section').classList.remove('hidden');
+        
+        document.getElementById('add_money_section').classList.add('hidden');
+        document.getElementById('cash_out_section').classList.add('hidden');
+        document.getElementById('latest_payment_section').classList.add('hidden');
+        document.getElementById('pay_bill_section').classList.add('hidden');
+    })
+
+// Get bonus.
+document.getElementById('get_bonus_btn')
+    .addEventListener('click', function(){
+        document.getElementById('get_bonus_section').classList.remove('hidden');
+        
+        document.getElementById('add_money_section').classList.add('hidden');
+        document.getElementById('cash_out_section').classList.add('hidden');
+        document.getElementById('latest_payment_section').classList.add('hidden');
+        document.getElementById('pay_bill_section').classList.add('hidden');
+    })
+
+// Pay bill.
+document.getElementById('pay_bill_btn')
+    .addEventListener('click', function(){
+        document.getElementById('pay_bill_section').classList.remove('hidden');
+        
+        document.getElementById('add_money_section').classList.add('hidden');
+        document.getElementById('cash_out_section').classList.add('hidden');
+        document.getElementById('get_bonus_section').classList.add('hidden');
+        document.getElementById('latest_payment_section').classList.add('hidden');
+    })
